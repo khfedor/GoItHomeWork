f10ec22ab3ef60369a266e249c9088146a513644
khrystynafedor <fxs1511@gmail.com>
2023-11-14 17:28:49 -0500

    Done

---
+++ src/main/java/hw11task1/Exercise.java
@@ -0,0 +1,30 @@
+package hw11task1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Exercise {
+    public List<String> oddValues(String url){
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(url))){
+            List<String> names = bufferedReader.lines().collect(Collectors.toList());
+            return IntStream.range(0, names.size()).filter(i->i%2==1).mapToObj(names::get).peek(System.out::println).collect(Collectors.toList());
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Exercise exercise = new Exercise();
+        exercise.oddValues("src/main/java/hw11task1/filenames.txt");
+    }
+}
+
+
